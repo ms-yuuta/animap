70642c0080263428741aa2470ea48e0d76a05711
Yuta Masuda <massu2000.1019@gmail.com>
2021-10-28 12:11:36 +0900

    ✨ main componentの生成（主に地図の表示）

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,33 @@
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { useMemo } from "react";
+import { GoogleMapComponent } from "./GoogleMapComponent";
+
+export const Main = () => {
+  const mapSettings = useMemo(() => {
+    const containerStyle = {
+      width: "100%",
+      height: "100vh",
+    };
+
+    const center = {
+      lat: 38.2,
+      lng: 139.77521,
+    };
+
+    return { containerStyle, center };
+  }, []);
+
+  return (
+    <main>
+      <LoadScript googleMapsApiKey="AIzaSyA0z5-r2o29M_pH7K3URo-mKOxiPJmWoCA">
+        <GoogleMap
+          mapContainerStyle={mapSettings.containerStyle}
+          center={mapSettings.center}
+          zoom={6}
+        >
+          <GoogleMapComponent />
+        </GoogleMap>
+      </LoadScript>
+    </main>
+  );
+};
