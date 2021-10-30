cb397cf1d7e3e3f54f709c8656493964fdce8db9
Yuta Masuda <massu2000.1019@gmail.com>
2021-10-30 16:06:11 +0900

    🐛 build時のtypeErrorの修正？　コンパイル時に'*.txs'の認識？

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
+declare module '*.tsx';
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
