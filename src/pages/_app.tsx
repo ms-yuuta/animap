0ec16ef417ead49500ea4cda56456c988adf6149
Yuta Masuda <massu2000.1019@gmail.com>
2021-10-31 13:56:07 +0900

    🐛 global css 読み込みを停止

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "styles/globals.css";
+// import "styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "@mui/material";
