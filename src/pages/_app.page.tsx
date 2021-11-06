cb224ce9b745c223b64335e23c7c15b54a543ac7
Yuta Masuda <massu2000.1019@gmail.com>
2021-11-06 12:56:45 +0900

    ➖ recoil

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,6 +1,5 @@
 import "styles/globals.css";
 import type { AppProps } from "next/app";
-import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "theme";
 import { SWRConfig } from "swr";
@@ -8,13 +7,11 @@ import { fetcher } from "./_app.hooks";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <SWRConfig value={{ fetcher }}>
-        <ThemeProvider theme={theme}>
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </SWRConfig>
-    </RecoilRoot>
+    <SWRConfig value={{ fetcher }}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SWRConfig>
   );
 }
 
