94cf60b7edc97561976088a1e910c83ed7517e7c
Yuta Masuda <massu2000.1019@gmail.com>
2021-11-17 14:17:07 +0900

    🔧 API_URLをenvに追加

--- src/pages/map.hooks.ts
+++ src/pages/map.hooks.ts
@@ -3,11 +3,11 @@ import { Fallback, Seichi } from "model";
 export const getStaticProps = async (): Promise<{
   props: { fallback: Fallback };
 }> => {
-  const SEICHI_API_URL = "https://jsondata.okiba.me/v1/json/VrDJ8210827043712";
+  const SEICHI_API_URL = `${process.env.NEXT_PUBLIC_API_URL}/VrDJ8210827043712`;
   const seichi = await fetch(SEICHI_API_URL);
   const seichiData: Seichi = await seichi.json();
 
-  const TITLE_API_URL = "https://jsondata.okiba.me/v1/json/yJlau210827043212";
+  const TITLE_API_URL = `${process.env.NEXT_PUBLIC_API_URL}/yJlau210827043212`;
   const title = await fetch(TITLE_API_URL);
   const titleData = await title.json();
 
