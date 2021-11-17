3d1f6e1c159f53818af063d9bc4d35c1238ca15f
Yuta Masuda <massu2000.1019@gmail.com>
2021-11-17 20:52:32 +0900

    ♻️

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,25 +1,29 @@
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, useLoadScript } from "@react-google-maps/api";
 import { VFC } from "react";
 import { useMapSettings } from "./map.hooks";
 
 type Props = {
   GoogleMapComponent: JSX.Element;
-}
+};
 
 export const Map: VFC<Props> = (props) => {
   const mapSettings = useMapSettings();
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY,
+  });
+
+  if (!isLoaded) return <div>loading....</div>;
+  if (loadError) return <div>Map cannot be loaded right now, sorry.</div>;
 
   return (
     <main>
-      <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}>
-        <GoogleMap
-          mapContainerStyle={mapSettings.containerStyle}
-          center={mapSettings.center}
-          zoom={6}
-        >
-          {props.GoogleMapComponent}
-        </GoogleMap>
-      </LoadScript>
+      <GoogleMap
+        mapContainerStyle={mapSettings.containerStyle}
+        center={mapSettings.center}
+        zoom={6}
+      >
+        {props.GoogleMapComponent}
+      </GoogleMap>
     </main>
   );
 };
