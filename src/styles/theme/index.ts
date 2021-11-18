6b2c5a15ae5fa9535b9bc6184029e90320e50cc4
Yuta Masuda <massu2000.1019@gmail.com>
2021-11-18 15:41:46 +0900

    💄 グローバルcssの変更

--- src/styles/theme/index.ts
+++ src/styles/theme/index.ts
@@ -1,10 +1,30 @@
 import { createTheme } from "@mui/material/styles";
 
 export const theme = createTheme({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        html {
+          margin: 0;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale:
+        }
+        body {
+          margin: 0;
+        }
+      `,
+    },
+  },
   palette: {
     primary: {
-      light: "#80cbc4",
-      main: "#26a69a",
+      light: "#5c7fbb",
+      main: "#3460AB",
+      dark: "#244377",
+    },
+    secondary: {
+      light: "#facb33",
+      main: "#F9BE00",
+      dark: "#ae8500",
     },
   },
   shape: {
@@ -12,7 +32,7 @@ export const theme = createTheme({
   },
   typography: {
     button: {
-      textTransform: "none"
-    }
-  }
+      textTransform: "none",
+    },
+  },
 });
