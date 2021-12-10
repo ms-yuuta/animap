6666a8bd0974a199e5585b5399d7b4bdd7885308
Yuta Masuda <massu2000.1019@gmail.com>
2021-12-10 12:10:51 +0900

    ♻️

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -9,14 +9,14 @@ import { fetcher } from "./_app.hooks";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig value={{ fetcher }}>
-      <ThemeProvider theme={theme}>
-        <div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <SWRConfig value={{ fetcher }}>
           <Component {...pageProps} />
-          <Toaster toastOptions={{ duration: 4000, style: { border: "1px solid #3460AB" } }} />
-        </div>
-      </ThemeProvider>
-    </SWRConfig>
+        </SWRConfig>
+        <Toaster toastOptions={{ duration: 4000, style: { border: "1px solid #3460AB" } }} />
+      </div>
+    </ThemeProvider>
   );
 }
 
