91d041d3e44523eed59d22b35a3b3839e58c64ed
Yuta Masuda <massu2000.1019@gmail.com>
2021-12-18 14:47:23 +0900

    📦 bundleAnalyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+const config = {
   swcMinify: true,
   reactStrictMode: true,
   pageExtensions: ["page.tsx", "page.ts"],
 };
+
+module.exports = withBundleAnalyzer(config);
